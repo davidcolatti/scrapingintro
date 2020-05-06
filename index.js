6afdb702e69f293a58b4887d00f94201aa0e6611
DavidColatti <davidcolatti@gmail.com>
2020-05-06 10:29:19 -0400

    intro

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 
 async function main() {
-	const html = await request.get('https://reactnativetutorial.net/css-selectors/lesson2.html');
+	const html = await request.get('https://reactnativetutorial.net/css-selectors/lesson6.html');
 
 	fs.writeFileSync('./test.html', html);
 	const $ = await cheerio.load(html);
 
-	const h2tags = [];
-	$('h2').each((index, element) => {
-		h2tags.push($(element).text());
+	// selecting element with class name
+	$('h2.red').each((index, element) => {
+		console.log($(element).text());
 	});
-	console.log(h2tags);
+
+	// selecting element with an attribute
+	$('[data-customer]').each((index, element) => {
+		console.log($(element).text());
+	});
 }
 
 main();
